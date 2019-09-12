ebab76be052ab587bff4dc1fe9cf3fd4d1c877cd
Brandon Liu <bdon@bdon.org>
2019-09-11 23:58:27 -0400

    add garmin nontabular format

--- osm_export_tool/nontabular.py
+++ osm_export_tool/nontabular.py
@@ -24,9 +24,11 @@ class Osmand:
 
     CLASSPATH = "{map_creator_dir}/OsmAndMapCreator.jar:{map_creator_dir}/lib/OsmAnd-core.jar:{map_creator_dir}/lib/*.jar"
 
-    def __init__(self,input_pbf,tempdir=None):
+    def __init__(self,input_pbf,map_creator_dir,tempdir=None,jvm_mem=[256,2048]):
         self.input_pbf = input_pbf
         self.tempdir = tempdir
+        self.jvm_mem = jvm_mem
+        self.map_creator_dir = map_creator_dir
 
     def run(self):
         pathlib.Path(join(self.tempdir,'osmand')).mkdir(parents=True, exist_ok=True)
@@ -41,8 +43,60 @@ class Osmand:
 
         subprocess.check_call([
             'java',
+            '-Xms{0}M'.format(self.jvm_mem[0]),
+            '-Xmx{0}M'.format(self.jvm_mem[1]),
             '-cp',
-            self.CLASSPATH.format(map_creator_dir='tools/OsmAndMapCreator-main'),
+            self.CLASSPATH.format(map_creator_dir=self.map_creator_dir),
             'net.osmand.util.IndexBatchCreator',
             join(self.tempdir,'batch.xml')
+        ])
+
+class Garmin:
+    """
+    Converts PBF to Garmin IMG format.
+
+    Splits pbf into smaller tiles, generates .img files for each split,
+    then patches the .img files back into a single .img file
+    suitable for deployment to a Garmin GPS unit.
+    """
+    def __init__(self,input_pbf,splitter_jar,mkgmap_jar,tempdir=None,jvm_mem=[256,2048]):
+        self.input_pbf = input_pbf
+        self.splitter_jar = splitter_jar
+        self.mkgmap_jar = mkgmap_jar
+        self.tempdir = tempdir
+        self.jvm_mem = jvm_mem
+
+    def run(self):
+        # NOTE: disabled poly bounds: see https://github.com/hotosm/osm-export-tool2/issues/248
+        subprocess.check_call([
+            'java',
+            '-Xms{0}M'.format(self.jvm_mem[0]),
+            '-Xmx{0}M'.format(self.jvm_mem[1]),
+            '-jar',
+            self.splitter_jar,
+            '--output-dir=' + self.tempdir,
+            self.input_pbf
+        ])
+
+        # Generate the IMG file.
+        # get the template.args file created by splitter
+        # see: http://wiki.openstreetmap.org/wiki/Mkgmap/help/splitter
+        subprocess.check_call([
+            'java',
+            '-Xms{0}M'.format(self.jvm_mem[0]),
+            '-Xmx{0}M'.format(self.jvm_mem[1]),
+            '-jar',
+            self.mkgmap_jar,
+            '--gmapsupp',
+            '--output-dir=' + self.tempdir,
+            '--description="HOT Export Garmin Map"',
+            '--mapname=80000111',
+            '--family-name="HOT Export Tool"',
+            '--family-id="2"',
+            '--series-name="HOT Export Tool"',
+            '--index',
+            '--route',
+            '--generate-sea=extend-sea-sectors',
+            '--draw-priority=100',
+            '--read-config={0}/template.args'.format(self.tempdir)
         ])
