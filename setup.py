2b771d7b1c7337f0da33274fcdba62ea3715b817
Brandon Liu <bdon@bdon.org>
2019-10-31 11:48:59 -0400

    0.0.17

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ requirements.append(['gdal~=' + gdal_version])
 
 setuptools.setup(
     name="osm-export-tool",
-    version="0.0.16",
+    version="0.0.17",
     author="Brandon Liu",
     author_email="brandon.liu@hotosm.org",
     description="Convert OpenStreetMap data into GIS and mobile mapping file formats.",
