e364032d840b269a6246742930f819ca5b17fd78
Brandon Liu <bdon@bdon.org>
2020-02-11 13:51:33 -0500

    0.0.23

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ requirements = [
 
 setuptools.setup(
     name="osm-export-tool",
-    version="0.0.22",
+    version="0.0.23",
     author="Brandon Liu",
     author_email="brandon.liu@hotosm.org",
     description="Convert OpenStreetMap data into GIS and mobile mapping file formats.",
