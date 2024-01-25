041863e691ca399d2473c54dc9a4dfb84518b37e
kshitijrajsharma <skshitizraj@gmail.com>
2024-01-25 18:46:24 +0545

    2.0.13

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = [
 
 setuptools.setup(
     name="osm-export-tool-python",
-    version="2.0.12",
+    version="2.0.13",
     author="Hot Tech Team",
     author_email="sysadmin@hotosm.org",
     description="Convert OpenStreetMap data into GIS and mobile mapping file formats.",
